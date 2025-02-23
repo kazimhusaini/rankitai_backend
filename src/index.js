d35a045111db515e02d98883057440bf63fea6e5
kazim husaini <kazimhusaini12@gmail.com>
2025-02-24 02:47:04 +0500

    add vercel file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// src/server.js
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
@@ -6,7 +7,7 @@ import helmet from 'helmet';
 import compression from 'compression';
 import rateLimiter from './middlewares/rateLimiter.js';
 import errorHandler from './middlewares/errorHandler.js';
-import { dbConnect } from './config/db.js';
+import { dbConnect } from './config/db.js';  // Make sure to destructure the import correctly
 import authRoutes from './routes/authRoutes.js';
 import contentRoutes from './routes/contentRoutes.js';
 import competitorRoutes from './routes/competitorRoutes.js';
@@ -17,6 +18,7 @@ import businessRoutes from './routes/businessRoutes.js';
 import localizationRoutes from './routes/localizationRoutes.js';
 import swaggerDocs from './docs/apiDocs.js';
 
+// Load environment variables
 dotenv.config();
 
 // Connect to the database
@@ -24,14 +26,8 @@ dbConnect();
 
 const app = express();
 
-const corsOptions = {
-    origin: 'https://rankitai-backend-noeo.vercel.app', // Replace with your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-};
-
 // Middleware setup
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
